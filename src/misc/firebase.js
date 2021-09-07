e016937ee974ed5dcd2fad1e3084298656429e0d
raghav <raghavsharma6996@gmail.com>
2021-09-07 16:09:39 +0530

    first commit

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+
+const config =  {
+        apiKey: "AIzaSyC4bXQIwRxypdASLfJ4ypNG5izr3fk1rA4",
+        authDomain: "chat-app-18f27.firebaseapp.com",
+        projectId: "chat-app-18f27",
+        storageBucket: "chat-app-18f27.appspot.com",
+        messagingSenderId: "1064996440256",
+        appId: "1:1064996440256:web:a46f3962846e2f2fad8525"
+      };
+      
+const app = firebase.initializeApp(config);
+
